221c77cb3635a7df57813f26d5aadeff9d1e56f0
Alina <alina84770@.com>
2025-05-07 17:52:41 +0500

    update code

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+let form=document.querySelector('.SignUpForm');
 let subBtn = document.querySelector(".subBtn");
 let nameError = document.querySelector(".nameError");
 let emailError = document.querySelector(".emailError");
 let passwordError = document.querySelector(".passwordError");
-
+let inputFields = document.querySelectorAll('input');
+let i = document.querySelectorAll('.icon');
 //form prevention
 subBtn.addEventListener("click", (e) => {
     e.preventDefault();
@@ -10,14 +12,19 @@ subBtn.addEventListener("click", (e) => {
         alert("Form Submitted Successfully");
 
         //clear input fields after form submission
-        document.querySelector(".name").value = "";
-        document.querySelector(".email").value = "";
-        document.querySelector(".password").value = "";
+        form.reset();
+        // document.querySelector(".name").value = "";
+        // document.querySelector(".email").value = "";
+        // document.querySelector(".password").value = "";
 
         //remove check icons after form submission
-        document.querySelector(".nameCheck").style.display = "none";
-        document.querySelector(".emailCheck").style.display = "none";
-        document.querySelector(".passwordCheck").style.display = "none";
+
+        for(let icon of i){
+            icon.style.display="none";
+        }
+        // document.querySelector(".nameCheck").style.display = "none";
+        // document.querySelector(".emailCheck").style.display = "none";
+        // document.querySelector(".passwordCheck").style.display = "none";
     }
 
 
@@ -28,94 +35,77 @@ subBtn.addEventListener("click", (e) => {
 //validate Name
 function validateName() {
     let name = document.querySelector(".name").value;
-    let icon = document.querySelector("i");
     if (!name) {
         nameError.innerText = "Name is required";
         nameError.previousElementSibling.classList.add("fa-xmark");
         return false;
     }
-    if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+    else if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
         nameError.innerText = "Write Full Name";
         nameError.previousElementSibling.classList.add("fa-xmark");
         return false
     }
-    nameError.innerText = "";
-    nameError.previousElementSibling.classList.add("fa-check");
-    return true;
+    else{
+        nameError.innerText = "";
+        nameError.previousElementSibling.classList.add("fa-check");
+        return true;
+    }
+    
 }
 
 
 //validate Email
 function validateEmail() {
     let email = document.querySelector(".email").value;
-    let icon = document.querySelector("i");
     if (!email) {
         emailError.innerText = "Email is required";
         emailError.previousElementSibling.classList.add("fa-xmark");
         return false
     }
-    if (!email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+    else if (!email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
         emailError.innerText = "Enter Valid Email";
         email.Error.previousElementSibling.classList.add("fa-xmark");
         return false
     }
-    emailError.innerText = "";
-    emailError.previousElementSibling.classList.add("fa-check");
-    return true
+    else{
+        emailError.innerText = "";
+        emailError.previousElementSibling.classList.add("fa-check");
+        return true
+    }
+    
 }
 
 
 //validate password
 function validatepassword() {
     let password = document.querySelector(".password").value;
-    let icon = document.querySelector("i");
     if (!password) {
         passwordError.innerText = "password is required";
         passwordError.previousElementSibling.classList.add("fa-xmark");
         return false
 
     }
-    if (!password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,30}$/)) {
+    else if (!password.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,30}$/)) {
         passwordError.innerHTML = "password should contain 1 uppercase,1 Lowercase,1 Digit <br>& 1 Alphabet";
         passwordError.previousElementSibling.classList.add("fa-xmark");
         return false
     }
-    passwordError.innerHTML = "";
-    passwordError.previousElementSibling.classList.add("fa-check");
-    return true
+    else{
+        passwordError.innerHTML = "";
+        passwordError.previousElementSibling.classList.add("fa-check");
+        return true;
+    }
+    
 }
 
-//Focus and Blur event on input field 'full Name'
-let name = document.querySelector(".name");
-name.addEventListener("focus", (e) => {
-    name.style.boxShadow = "0 0 7px lightskyblue";
-});
 
-//Blur Event
-name.addEventListener("blur", (e) => {
-    name.style.boxShadow = "none";
-    console.log("focus losses");
-});
+//focus and blur event
+for(let field of inputFields){
+    field.addEventListener('focus',(e)=>{
+        e.target.style.boxShadow = "0 0 7px lightskyblue";
+    })
+    field.addEventListener('blur',(e)=>{
+        e.target.style.boxShadow = "none"; 
+    })
+}
 
-//Focus and Blur event on input field 'Email'
-let email = document.querySelector(".email");
-email.addEventListener("focus", (e) => {
-    email.style.boxShadow = "0 0 7px lightskyblue";
-});
-
-//Blur Event
-email.addEventListener("blur", (e) => {
-    email.style.boxShadow = "none";
-});
-
-
-//Focus and Blur event on input field 'password'
-let password = document.querySelector(".password");
-password.addEventListener("focus", (e) => {
-    password.style.boxShadow = "0 0 7px lightskyblue";
-});
-
-//Blur Event
-password.addEventListener("blur", (e) => {
-    password.style.boxShadow = "none";
-});
